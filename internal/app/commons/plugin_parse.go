74c823f530dcc2d94a3701cc8da177fc5e144962
suhang12332 <1552899301@qq.com>
2024-07-12 09:34:31 +0800

    Remove unused imports in plugin_parse.go
    The "reflect" and "qqbot-reconstruction/internal/app/plugins" imports were removed from the plugin_parse.go file. They were previously imported but not used in the code, creating unnecessary complexity.

--- internal/app/commons/plugin_parse.go
+++ internal/app/commons/plugin_parse.go
@@ -2,9 +2,7 @@
 package commons
 
 import (
-    "qqbot-reconstruction/internal/app/plugins"
     "qqbot-reconstruction/internal/pkg/variable"
-    "reflect"
 )
 
 func (r *PluginRegistry)PluginScanner(plugin *variable.PluginInfo)  {
